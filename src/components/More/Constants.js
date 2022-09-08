78a49a63c7234d9a6c911c016db80b68e70415f1
stephanbazbaz <stephan@pwm.co.il>
2022-09-08 11:08:48 +0300

    remoced animation

--- src/components/More/Constants.js
+++ src/components/More/Constants.js
@@ -1,1 +1,10 @@
-export const DummyText = 'Lorem ipsum dolor sit amet. Ab culpa optio eos quidem suscipit aut ...'
+export const DummyText =
+  "Lorem ipsum dolor sit amet. Ab culpa optio eos quidem suscipit aut ..."
+export const cardArr = Array(4)
+  .fill("")
+  .map((_, i) => {
+    return {
+      text: DummyText,
+      title: `מאמר ${i + 1}`,
+    }
+  })
--- src/components/More/More.js
+++ src/components/More/More.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import './More.scss'
-import { DummyText } from './Constants'
-import ArticleCard from '../Common/ArticleCard/ArticleCard'
+import React from "react"
+import "./More.scss"
+import { cardArr } from "./Constants"
+import ArticleCard from "../Common/ArticleCard/ArticleCard"
 const More = () => {
-    const cardArr = Array(4).fill('').map((_, i) => {
-        return {
-            text: DummyText,
-            title: `מאמר ${i + 1}`
-        }
-    })
-    return (
-        <div className='more'>
-            <div className='shadow-contanier'>
-                <h1 title="למידע נוסף על נעמי היימן איתי" className="title">חומר למשחבה</h1>
-                <div className='more-card-row'>
-                    {cardArr?.map((item, idx) => <ArticleCard
-                        key={`card-${idx}`}
-                        title={item.title}
-                        text={item.text}
-                    />)}
-                </div>
-            </div>
+  // const cardArr = Array(4).fill('').map((_, i) => {
+  //     return {
+  //         text: DummyText,
+  //         title: `מאמר ${i + 1}`
+  //     }
+  // })
+  return (
+    <div className="more">
+      <div className="shadow-contanier">
+        <h1 title="למידע נוסף על נעמי היימן איתי" className="title">
+          חומר למשחבה
+        </h1>
+        <div className="more-card-row">
+          {cardArr?.map((item, idx) => (
+            <ArticleCard
+              key={`card-${idx}`}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default More
