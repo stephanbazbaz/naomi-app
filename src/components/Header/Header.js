226f9e9d50f99d2ffebeb11862c2e902e8889b6b
stephanbazbaz <stephan@pwm.co.il>
2022-09-30 19:05:23 +0300

    redirect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,49 @@
-import React from 'react'
-import { Tab, Box, } from '@mui/material';
-import { TabContext, TabList } from '@mui/lab';
-import { MORE } from '../../utils/routes.js'
-import { useNavigate } from 'react-router-dom'
-import { ROUTES_ARR } from './Constants'
-import { ReactComponent as LightBolb } from '../../Assets/light-bulb_2.svg'
-import './Header.scss'
+import React from "react"
+import { Tab, Box } from "@mui/material"
+import { TabContext, TabList } from "@mui/lab"
+import { MORE } from "../../utils/routes.js"
+import { useNavigate } from "react-router-dom"
+import { ROUTES_ARR } from "./Constants"
+import { ReactComponent as LightBolb } from "../../Assets/light-bulb_2.svg"
+import "./Header.scss"
 const Header = () => {
-  const url = window.location.href.split('/').pop()
-  const [value, setValue] = React.useState(`/${url}`);
+  const url = window.location.href.split("/").pop()
+  const [value, setValue] = React.useState(`/${url}`)
   let history = useNavigate()
   const handleChange = (event, newValue) => {
     history(newValue)
-    setValue(newValue);
-  };
+    setValue(newValue)
+  }
 
   return (
-    <div className='header'>
+    <div className="header">
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList classes={{
-            indicator: 'tabs-indicator'
-          }} centered onChange={handleChange}>
-            {ROUTES_ARR.map((item, idx) =>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList
+            classes={{
+              indicator: "tabs-indicator",
+            }}
+            centered
+            onChange={handleChange}
+          >
+            {ROUTES_ARR.map((item, idx) => (
               <Tab
                 classes={{
-                  selected: 'tabs-selected'
+                  selected: "tabs-selected",
                 }}
-                key={`${'tab' + idx}`}
-                label={item.route !== MORE ? item.name :
-                  <div>{item.name} <LightBolb style={{ width: '10px' }} /></div>}
-                value={item.route} />)}
+                key={`${"tab" + idx}`}
+                label={
+                  item.route !== MORE ? (
+                    item.name
+                  ) : (
+                    <div>
+                      {item.name} <LightBolb style={{ width: "10px" }} />
+                    </div>
+                  )
+                }
+                value={item.route}
+              />
+            ))}
           </TabList>
         </Box>
       </TabContext>
